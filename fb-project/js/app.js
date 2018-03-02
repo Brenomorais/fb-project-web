7e71bab8aee48668c00c0f153dd7095c62604418
Breno Morais <breno.lopes.morais@hotmail.com>
2018-03-02 02:13:04 -0300

    added css,js and others cdn

---
+++ fb-project/js/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+      apiKey: "AIzaSyALGNvH5kssfpHGU1MdZNzJ-FhcITmEtdY",
+      authDomain: "fb-project-a54c3.firebaseapp.com",
+      databaseURL: "https://fb-project-a54c3.firebaseio.com",
+      projectId: "fb-project-a54c3",
+      storageBucket: "fb-project-a54c3.appspot.com",
+      messagingSenderId: "707226799269"
+  };
+  firebase.initializeApp(config);
